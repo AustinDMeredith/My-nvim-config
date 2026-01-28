a9259f380614dc3045511ab1d04d11fe6cae358c
Austin <austin09017@gmail.com>
2026-01-28 00:11:56 -0600

    changed some keybinds

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -4,6 +4,5 @@ vim.keymap.set(
   "<cmd>:Neotree toggle<CR>",
   { desc = "Neo-tree: Toggle file explorer" }
 )
-vim.keymap.set("n", "<C-g>", "<cmd>LazyGit<CR>", { desc = "LazyGit" })
 --vim.keymap.set("n", "<C-t>", "<cmd>ToggleTerm<CR>", { desc = "Terminal" })
 --vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -15,6 +15,21 @@ return {
         persist_size = true,
         close_on_exit = true,
       })
+      local Terminal = require("toggleterm.terminal").Terminal
+
+      local lazygit = Terminal:new({
+        cmd = "lazygit",
+        hidden = true,
+        direction = "float",
+        float_opts = { border = "rounded" },
+      })
+
+      local function toggle_lazygit()
+        lazygit:toggle()
+      end
+
+      -- Map in BOTH normal and terminal mode
+      vim.keymap.set({ "n", "t" }, "<C-g>", toggle_lazygit, { desc = "Toggle LazyGit", silent = true })
     end,
   },
 }
