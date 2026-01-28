4e5482d89daf26858d4843594f315416fd6d96e3
Austin <austin09017@gmail.com>
2026-01-28 13:25:20 -0600

    merged the new python lsp into main and resolved desktop path conflict

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
   "nvim-treesitter/nvim-treesitter", 
   build = ":TSUpdate", 
   config = function ()
-    require("nvim-treesitter.configs").setup({
+    require("nvim-treesitter.config").setup({
       ensure_installed = { "lua", "bash", "python", "javascript", "json", "yaml", "markdown", "cpp", "java" },
       highlight = { enable = true },
       indent = { enable = true },
