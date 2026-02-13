678d16314ff9882dbc68afe48713791e5d9c0ac6
Austin <austin09017@gmail.com>
2026-02-13 16:02:19 -0600

    added some keybinds

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -4,5 +4,11 @@ vim.keymap.set(
   "<cmd>:Neotree toggle<CR>",
   { desc = "Neo-tree: Toggle file explorer" }
 )
+
+-- go to definition
+vim.keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>")
+
+-- open definition in split window
+vim.keymap.set("n", "gD", "<cmd>vsplit | Telescope lsp_definitions<CR>")
 --vim.keymap.set("n", "<C-t>", "<cmd>ToggleTerm<CR>", { desc = "Terminal" })
 --vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
